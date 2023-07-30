2899c14f98ed61951a2f8475ad31890945f343ba
Saiteja <Saitejacp210@gmail.com>
2023-07-30 14:25:18 +0530

    Reomved the Unwanted code from the src/components/Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,30 +6,27 @@ import linkedin from './images/linkedin.svg'
 import insta from './images/instagram.svg'
 import Socials from './Socials'
 import { useState } from 'react'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 function Contact() {
 
-    //    let userState = false;
-    //     let navigate = useNavigate();
-        const {register,handleSubmit,formState:{errors}}= useForm()
-    // //    console.log(props)
-        let [msg,setMsg]= useState([]);
-     const onFormSubmit = (obj)=>{
-         handleSubmit(msg.push(obj));
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    let [msg, setMsg] = useState([]);
+    const onFormSubmit = (obj) => {
+        handleSubmit(msg.push(obj));
         console.log(msg);
-    //     axios.post("http://localhost:3999/admin/signup",obj)
-    //     .then((res)=>{
-    //         //if(res.status===201){
-    //             if(res.data?.Message==="User Already Exists") userState=true;
-    //             else userState = false;
-    //             setMsg(res.data?.Message);
-    //             console.log(res);
-    //             // props.fun();
-    //             //navigate("/admin")
-    //         //}
-    //     })
-    //     .catch((err)=>console.log(err))
-    //         console.log(obj)
+        //     axios.post("http://localhost:3999/admin/signup",obj)
+        //     .then((res)=>{
+        //         //if(res.status===201){
+        //             if(res.data?.Message==="User Already Exists") userState=true;
+        //             else userState = false;
+        //             setMsg(res.data?.Message);
+        //             console.log(res);
+        //             // props.fun();
+        //             //navigate("/admin")
+        //         //}
+        //     })
+        //     .catch((err)=>console.log(err))
+        //         console.log(obj)
     }
     return (
         <div className='container parent'>
@@ -38,27 +35,27 @@ function Contact() {
 
             <form name="form" role="form" className="login-form" onSubmit={handleSubmit(onFormSubmit)}>
                 <div class="form-floating mb-3" style={{ marginLeft: '20rem' }}>
-                    <input className="form-control" type="email" style={{ width: '70%' }} placeholder="Email id" {...register("email", {required:true,minLength:'8'})}/>
+                    <input className="form-control" type="email" style={{ width: '70%' }} placeholder="Email id" {...register("email", { required: true, minLength: '8' })} />
                     <label for="floatingInput">Email Id</label>
-                    {errors.email?.type==='minLength' && <p className="text-danger">*Email Id must be atleast 8 characters</p>}
-                    {errors.email?.type==='required' && <p className="text-danger">*Email Id is required</p>}
+                    {errors.email?.type === 'minLength' && <p className="text-danger">*Email Id must be atleast 8 characters</p>}
+                    {errors.email?.type === 'required' && <p className="text-danger">*Email Id is required</p>}
                 </div>
                 <br />
                 <div class="form-floating mb-3" style={{ marginLeft: '20rem' }}>
-                    <input className="form-control" type="text" style={{ width: '70%' }} placeholder="Subject" {...register("subject", {required:true,minLength:'10'})}/>
+                    <input className="form-control" type="text" style={{ width: '70%' }} placeholder="Subject" {...register("subject", { required: true, minLength: '10' })} />
                     <label for="floatingInput">Subject</label>
-                    {errors.subject?.type==='minLength' && <p className="text-danger">*subject must be atleast 10 characters</p>}
-                    {errors.subject?.type==='required' && <p className="text-danger">*subject is required</p>}
+                    {errors.subject?.type === 'minLength' && <p className="text-danger">*subject must be atleast 10 characters</p>}
+                    {errors.subject?.type === 'required' && <p className="text-danger">*subject is required</p>}
                 </div>
                 <br />
                 <div class="form-floating mb-3" style={{ marginLeft: '20rem' }}>
                     {/* <textarea className="form-control"/> </textarea> */}
-                    <textarea class="form-control" id="exampleFormControlTextarea1" style={{ width: '70%' }} rows="3" {...register("message", {required:true,minLength:'30'})}></textarea>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" style={{ width: '70%' }} rows="3" {...register("message", { required: true, minLength: '30' })}></textarea>
                     <label for="floatingInput">Message</label>
                     {/* <input className="form-control" type="password" placeholder="Password"  {...register("password", {required:true,minLength:'8'})}/> */}
                     {/* <label for="floatingInput">Password</label> */}
-                    {errors.message?.type==='minLength' && <p className="text-danger">*Message must be atleast 30 characters</p>}
-                    {errors.message?.type==='required' && <p className="text-danger">*Message is required</p>}
+                    {errors.message?.type === 'minLength' && <p className="text-danger">*Message must be atleast 30 characters</p>}
+                    {errors.message?.type === 'required' && <p className="text-danger">*Message is required</p>}
                 </div>
                 <button type="submit" class="btn btn-primary">SIGN IN</button>
                 {/* { userState ? <p className="text-danger">{msg}</p>: <p className="text-success">{msg}</p>} */}
@@ -82,7 +79,7 @@ function Contact() {
             </div>
             <button className='btn btn-success'>Send</button>
         </form> */}
-        <Socials className='bottom' style={{position:'absolute', bottom:'0px'}}/>
+            <Socials className='bottom' style={{ position: 'absolute', bottom: '0px' }} />
         </div>
     )
 }
